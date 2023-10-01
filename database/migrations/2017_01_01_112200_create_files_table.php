04c1238e698cb10ae8abeebf5330a7f4d9ac0150
Curtis Delicata <curtis.delicata@outlook.com>
2023-10-01 18:16:04 +0100

    Update 2017_01_01_112200_create_files_table.php

--- database/migrations/2017_01_01_112200_create_files_table.php
+++ database/migrations/2017_01_01_112200_create_files_table.php
@@ -23,7 +23,7 @@ return new class extends Migration
 
             $table->boolean('is_public');
 
-            $table->integer('created_by')->unsigned()->nullable();
+            $table->bigInteger('created_by')->unsigned()->nullable();
             $table->foreign('created_by')->references('id')->on('users');
 
             $table->timestamps();
